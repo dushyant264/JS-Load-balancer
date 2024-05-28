e7ebe9c4227c662db22b9ac5db11fc7d73588ed0
dushyant264 <ddushyant89@gmail.com>
2024-05-29 00:35:37 +0530

    try new

--- load.js
+++
@@ -1,7 +0,0 @@
-
-const load = http.createServer((req, res) => {
-    // Load balancer
-    console.log('Request received by Load Balancer');
-    // Forward the request to the server
-    proxy.web(req, res, { target: 'http://localhost:3000' });
-});
